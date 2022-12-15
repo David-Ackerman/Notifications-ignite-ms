2322223f11d9ddf183a7fc8cab625d9af32d1128
David-Ackerman <david_pereira-16@hotmail.com>
2022-12-14 22:22:41 -0300

    finish class two: improve code structure and creat unit test

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from './infra/http/http.module';
+import { PrismaService } from './infra/database/prisma/prisma.service';
+import { DatabaseModule } from './infra/database/database.module';
+
+@Module({
+  imports: [HttpModule, DatabaseModule],
+})
+export class AppModule {}
---
+++ src/infra/database/prisma/repositories/prisma-notifications-repositories.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../application/entities/notification';
+import { NotificationRepository } from '../../../../application/repositories/notification-repositories';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        createdAt: notification.createdAt,
+        readAt: notification.readAt,
+      },
+    });
+  }
+}
